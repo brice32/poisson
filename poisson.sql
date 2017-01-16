ce606273f6e91196ff3cf412e77f2e8a0ed59bcf
brice32 <wangyc32@hotmail.com>
2017-01-16 05:26:53 +0100

    sql update

--- poisson.sql
+++ poisson.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Jeu 12 Janvier 2017 à 19:15
+-- Généré le :  Lun 16 Janvier 2017 à 05:26
 -- Version du serveur :  5.6.26
 -- Version de PHP :  5.6.12
 
@@ -27,11 +27,20 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `diffuser` (
-  `ID_DIFFUSION` bigint(4) NOT NULL,
-  `ID_NEWS` bigint(4) NOT NULL,
-  `ORDRE` int(2) NOT NULL,
-  `TEMPSDIFFUSION` int(3) NOT NULL
+  `id_diffusion` bigint(4) NOT NULL,
+  `id_news` bigint(4) NOT NULL,
+  `ordre` int(2) NOT NULL,
+  `tempsdiffusion` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `diffuser`
+--
+
+INSERT INTO `diffuser` (`id_diffusion`, `id_news`, `ordre`, `tempsdiffusion`) VALUES
+(1, 1, 1, 30),
+(1, 2, 3, 30),
+(1, 4, 2, 30);
 
 -- --------------------------------------------------------
 
@@ -40,9 +49,16 @@ CREATE TABLE IF NOT EXISTS `diffuser` (
 --
 
 CREATE TABLE IF NOT EXISTS `diffusion` (
-  `ID` bigint(4) NOT NULL,
-  `DERNIEREMODIFICATION` datetime NOT NULL
+  `id` bigint(4) NOT NULL,
+  `dernieremodification` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `diffusion`
+--
+
+INSERT INTO `diffusion` (`id`, `dernieremodification`) VALUES
+(1, '2017-01-16 05:25:05');
 
 -- --------------------------------------------------------
 
@@ -51,9 +67,19 @@ CREATE TABLE IF NOT EXISTS `diffusion` (
 --
 
 CREATE TABLE IF NOT EXISTS `image` (
-  `ID` bigint(4) NOT NULL,
-  `CHEMIN` text NOT NULL
+  `id` bigint(4) NOT NULL,
+  `chemin` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `image`
+--
+
+INSERT INTO `image` (`id`, `chemin`) VALUES
+(1, 'images/images2017/1.jpg'),
+(2, 'images/images2017/2.jpg'),
+(3, 'images/images2017/3.jpg'),
+(4, 'images/images2017/4.jpg');
 
 -- --------------------------------------------------------
 
@@ -62,19 +88,29 @@ CREATE TABLE IF NOT EXISTS `image` (
 --
 
 CREATE TABLE IF NOT EXISTS `news` (
-  `ID` bigint(4) NOT NULL,
-  `ID_UTILISATEUR` smallint(10) NOT NULL,
-  `ID_IMAGE` bigint(4) NOT NULL,
-  `TITRE` char(32) NOT NULL,
-  `TEXTE` text NOT NULL,
-  `TAILLEFONT` int(3) NOT NULL,
-  `TAILLEBANDEAU` int(3) NOT NULL,
-  `COULEURFONT` char(7) NOT NULL,
-  `COULEURBANDEAU` char(12) NOT NULL,
-  `DATEHEUREMODFICATION` datetime NOT NULL,
-  `DATEHEURECREATION` char(32) NOT NULL,
-  `POSITIONBANDEAU` char(32) NOT NULL
+  `id` bigint(4) NOT NULL,
+  `id_utilisateur` smallint(10) NOT NULL,
+  `id_image` bigint(4) NOT NULL,
+  `titre` char(32) NOT NULL,
+  `texte` text NOT NULL,
+  `taillefont` int(3) NOT NULL,
+  `taillebande` int(3) NOT NULL,
+  `couleurfont` char(7) NOT NULL,
+  `couleurbandeau` char(12) NOT NULL,
+  `dateheuremodification` datetime NOT NULL,
+  `dateheurecreation` char(32) NOT NULL,
+  `positionbandeau` char(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `news`
+--
+
+INSERT INTO `news` (`id`, `id_utilisateur`, `id_image`, `titre`, `texte`, `taillefont`, `taillebande`, `couleurfont`, `couleurbandeau`, `dateheuremodification`, `dateheurecreation`, `positionbandeau`) VALUES
+(1, 6, 1, 'Aircafte carrier', 'Aircafte carrier text', 10, 10, 'gris', 'gris', '2017-01-01 00:00:00', '2017-01-01 00:00:00', 'top'),
+(2, 7, 2, 'test02', 'test02 texte', 10, 10, 'gris', 'gris', '2017-01-01 00:00:00', '2017-01-01 00:00:00', 'left'),
+(3, 7, 4, 'paris', 'paris texte', 10, 10, 'gris', 'gris', '2017-01-26 00:00:00', '2017-01-26 00:00:00', 'left'),
+(4, 6, 3, 'bordeaux', 'paris texte', 10, 10, 'gris', 'gris', '2017-01-26 00:00:00', '2017-01-26 00:00:00', 'left');
 
 -- --------------------------------------------------------
 
@@ -99,7 +135,7 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
 --
 
 INSERT INTO `utilisateurs` (`id`, `nom`, `prenom`, `email`, `login`, `motdepasse`, `creation`, `connexion`, `admin`) VALUES
-(6, 'wang', 'yuchen', 'wangyc32@gmail.com', 'test01', '39dd1b0e4356a97f2fbdf14f7858dfd49b524ba4b4ba97bdc895496074800a6c', '2017-01-12 18:14:34', '0000-00-00 00:00:00', 0),
+(6, 'wang', 'yuchen', 'wangyc32@gmail.com', 'test01', '39dd1b0e4356a97f2fbdf14f7858dfd49b524ba4b4ba97bdc895496074800a6c', '2017-01-12 18:14:34', '0000-00-00 00:00:00', 1),
 (7, 'willian', 'ruchaud', 'ruchaud@3il.fr', 'test02', 'fb5380fc71a98ce78215db372b0b81595d770ad3900978cf3f933b4863d87496', '2017-01-12 18:15:11', '0000-00-00 00:00:00', 0);
 
 --
@@ -110,29 +146,29 @@ INSERT INTO `utilisateurs` (`id`, `nom`, `prenom`, `email`, `login`, `motdepasse
 -- Index pour la table `diffuser`
 --
 ALTER TABLE `diffuser`
-  ADD PRIMARY KEY (`ID_DIFFUSION`,`ID_NEWS`),
-  ADD KEY `I_FK_DIFFUSER_DIFFUSION` (`ID_DIFFUSION`),
-  ADD KEY `I_FK_DIFFUSER_NEWS` (`ID_NEWS`);
+  ADD PRIMARY KEY (`id_diffusion`,`id_news`),
+  ADD KEY `I_FK_DIFFUSER_DIFFUSION` (`id_diffusion`),
+  ADD KEY `I_FK_DIFFUSER_NEWS` (`id_news`);
 
 --
 -- Index pour la table `diffusion`
 --
 ALTER TABLE `diffusion`
-  ADD PRIMARY KEY (`ID`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `image`
 --
 ALTER TABLE `image`
-  ADD PRIMARY KEY (`ID`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `news`
 --
 ALTER TABLE `news`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `I_FK_NEWS_UTILISATEURS` (`ID_UTILISATEUR`),
-  ADD KEY `I_FK_NEWS_IMAGE` (`ID_IMAGE`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `I_FK_NEWS_UTILISATEURS` (`id_utilisateur`),
+  ADD KEY `I_FK_NEWS_IMAGE` (`id_image`);
 
 --
 -- Index pour la table `utilisateurs`
@@ -157,14 +193,8 @@ ALTER TABLE `utilisateurs`
 -- Contraintes pour la table `diffuser`
 --
 ALTER TABLE `diffuser`
-  ADD CONSTRAINT `FK_DIFFUSER_DIFFUSION` FOREIGN KEY (`ID_DIFFUSION`) REFERENCES `diffusion` (`ID`),
-  ADD CONSTRAINT `FK_DIFFUSER_NEWS` FOREIGN KEY (`ID_NEWS`) REFERENCES `news` (`ID`);
-
---
--- Contraintes pour la table `news`
---
-ALTER TABLE `news`
-  ADD CONSTRAINT `FK_NEWS_IMAGE` FOREIGN KEY (`ID_IMAGE`) REFERENCES `image` (`ID`);
+  ADD CONSTRAINT `FK_DIFFUSER_DIFFUSION` FOREIGN KEY (`id_diffusion`) REFERENCES `diffusion` (`id`),
+  ADD CONSTRAINT `FK_DIFFUSER_NEWS` FOREIGN KEY (`id_news`) REFERENCES `news` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
